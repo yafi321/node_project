a728c9c3df2096bb74e641cfd6b9e97cd368addf
yl <y0504104974@gmail.com>
2025-03-24 23:08:15 +0200

    image update2

--- routers/product.js
+++ routers/product.js
@@ -7,9 +7,9 @@ const productRouter = Router();
 productRouter.get("/", getAllProducts);
 productRouter.get("/totalPages", getTotalPages);
 productRouter.get("/:id", getById);
-productRouter.delete("/:id",checkManager,uploadImage, deleteById);
+productRouter.delete("/:id",checkManager, deleteById);
 productRouter.put("/:id",checkManager,uploadImage, updateById);
-productRouter.post("/",checkManager, add);
+productRouter.post("/",checkManager,uploadImage, add);
 
 
 export default productRouter;
